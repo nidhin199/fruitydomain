5549c4c96700d84b533e187717ab99a780bd9b24
nidhin <nidhinjs994@gmail.com>
2023-04-18 12:30:01 +0530

    created login and register

--- fruitydomain/home/views.py
+++ fruitydomain/home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import auth
 
 
 def index(request):
@@ -7,5 +8,32 @@ def index(request):
 
 def test(request):
     return render(request,'text.html',{'val':'java'})
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
+
+def loginsub(request):
+    uname=request.GET['uname']
+    pname=request.GET['pname']
+    user=auth.authenticate(username=uname,password=pname)
+    if user:
+        auth.login(request,user)
+        return redirect('/')
+
+    return render(request,'text.html',{'val':user})
+
+def registersub(request):
+    uname=request.GET['uname']
+    fname=request.GET['fname']
+    lname=request.GET['lname']
+    ename=request.GET['ename']
+    pname=request.GET['pname']
+    rpname=request.GET['rpname']
+    return render(request,'text.html',{'val':uname})
+
+    
 
 # Create your views here.
